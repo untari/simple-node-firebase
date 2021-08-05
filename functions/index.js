9fb886a8a27a360372c7fd2c096dfc00094109da
untari <sastraembun0306@gmail.com>
2021-08-05 13:12:40 +0700

    Create and test an HTTPS function for the Hosting site

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require("firebase-functions");
+const express = require('express');
+const app = express();
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+  res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+});
+
