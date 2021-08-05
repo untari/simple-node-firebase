24c4199743e8b401add19b677a5a847aac94cae0
untari <sastraembun0306@gmail.com>
2021-08-05 15:03:51 +0700

    Add the first endpoint to serve the index of the website using express.js

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,26 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+
 const express = require('express');
 const app = express();
 
-exports.bigben = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
+const cors = require('cors')({origin: true});
+app.use(cors);
+
+
+app.get('/', (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+  res.send(`
+    <!doctype html>
     <head>
       <title>Time</title>
+      <link rel="stylesheet" href="/style.css">
+      <script src="/script.js"></script>
     </head>
     <body>
-      ${'BONG '.repeat(hours)}
+      <p>In London, the clock strikes:
+        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+      <button onClick="refresh(this)">Refresh</button>
     </body>
   </html>`);
 });
